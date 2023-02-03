d06c10ae1c90c539e07b7643943af8055f52b4d2
Amy Spears <amspears007@gmail.com>
2023-02-02 17:07:52 -0700

    Add first passing tests in Node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,5 +3,9 @@ require './lib/node'
 RSpec.describe Node do
   it 'is an instance of node and has a slot for data and next node' do
     node = Node.new("plop")
+
+    expect(node).to be_an_instance_of(Node)
+    expect(node.data).to eq("plop")
+    expect(node.next_node).to eq(nil)
   end
 end
